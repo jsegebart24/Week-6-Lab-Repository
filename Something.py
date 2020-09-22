bb5cfc48a02a01fee0795f24d4b3824973d691d4
johnsegebart24 <jes354@pitt.edu>
2020-09-22 16:32:18 -0400

    wrote the code that compares the user's birth year and assigns a gen type

--- Something.py
+++ Something.py
@@ -1,1 +1,10 @@
-name = input("Enter your name")
+name = input("Enter your name:")
+print(name)
+
+genYear = int(input("Enter your birth year:"))
+if  1965 <= genYear => 1979:
+    genType = "Baby Boomer"
+elif 1980 <= genYear => 1994:
+    genType = "Millennial"
+elif 1995 <= genYear => 2012: 
+    genType = "Gen Z"
